78639c6d442033f6d8b2b8bc8e49ab8b72bcf9a0
umayanga1997 <umayanga.avi@gmail.com>
2023-08-01 19:00:27 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [currentState, setCurrentState] = useState("");
 
+  // Without dependencies
+  useEffect(() => {
+    // Console Message
+    console.log(
+      "The useEffect runs with null dependencies!, runs on every render."
+    );
+  });
+
+  // With empty Array
+  useEffect(() => {
+    // Console Message
+    console.log(
+      "The useEffect runs with The Empty Array!, runs on first render."
+    );
+  }, []);
+
+  // With State or Prop value
+  useEffect(() => {
+    // Console Message
+    console.log(
+      "The useEffect runs with the State value!, runs when the State value updates."
+    );
+  }, [currentState]);
+
   return (
     <div className="container">
       <p>{currentState}</p>
-      <button onClick={() => setCurrentState("State was updated!")}>
+      <button
+        onClick={() =>
+          setCurrentState("State was updated with: " + Math.random())
+        }
+      >
         Click
       </button>
     </div>
